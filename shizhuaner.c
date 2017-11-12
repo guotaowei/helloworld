a2497651a5e53d4af92824ea9516c0b06e4cec2b
guotaowei <740559313@qq.com>
2017-11-12 21:00:50 +0800

    十进制转二进制程序

---
+++ shizhuaner.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+void main()
+{
+	int shi,zhong,jian,er=0;
+	printf("ten to two\n");
+	scanf("shu ru shi jin zhi %d",&shi);
+	zhong=shi;
+	while(zhong!=0)
+	{
+		jian=zhong%2;
+		er=er*10+jian;
+		zhong=zhong/2;
+	}
+	printf("%d,%d\n",shi,er);
+}
+	
