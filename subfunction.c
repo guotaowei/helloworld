b1dd66ef8cd15847b71ca58fbc2c087ff7abcbec
guotaowei <740559313@qq.com>
2017-11-28 22:22:04 +0800

    子函数库

--- subfunction.c
+++ subfunction.c
@@ -1,11 +1,11 @@
-int recursion_sequence1(int a[],int b[],int start,int end)
+int recursion_sort1(int a[],int b[],int start,int end)//递归排序法，小→大
 {
 	int middle=(start+end)/2;
 	//设置返回条件
 	if(middle>=end||middle<start)
 		return 0;
-	recursion_sequence1(a,b,start,middle);
-	recursion_sequence1(a,b,middle+1,end);	
+	recursion_sort1(a,b,start,middle);
+	recursion_sort1(a,b,middle+1,end);	
 	int m=middle+1,i=start,j=start;
 	//分组比较，当任一组下标超出，结束循环
 	while(j<=middle&&m<=end)
@@ -44,14 +44,14 @@ int recursion_sequence1(int a[],int b[],int start,int end)
 	for(j=start;j<=end;j++)
 		a[j]=b[j];
 }
-int recursion_sequence2(int a[],int b[],int start,int end)
+int recursion_sort2(int a[],int b[],int start,int end)//递归排序法，大→小
 {
 	int middle=(start+end)/2;
 	//设置返回条件
 	if(middle>=end||middle<start)
 		return 0;
-	recursion_sequence2(a,b,start,middle);
-	recursion_sequence2(a,b,middle+1,end);	
+	recursion_sort2(a,b,start,middle);
+	recursion_sort2(a,b,middle+1,end);	
 	int m=middle+1,i=start,j=start;
 	//分组比较，当任一组下标超出，结束循环
 	while(j<=middle&&m<=end)
@@ -89,4 +89,67 @@ int recursion_sequence2(int a[],int b[],int start,int end)
 	//把排好序的值重新赋给数组a
 	for(j=start;j<=end;j++)
 		a[j]=b[j];
+}
+int sequence_sort1(int a[],int start,int end)//顺序排序法，小→大
+{
+	int i,j,k;
+	for(i=start;i<end;i++)
+		for(j=i+1;j<=end;j++)
+		{
+			if(a[i]>a[j])
+			{
+				k=a[i];
+				a[i]=a[j];
+				a[j]=k;
+			}
+		}
+}
+void sequence_sort2(int a[],int start,int end)//顺序排序法，大→小
+{
+	int i,j,k;
+	for(i=start;i<end;i++)
+		for(j=i+1;j<=end;j++)
+		{
+			if(a[i]<a[j])
+			{
+				k=a[i];
+				a[i]=a[j];
+				a[j]=k;
+			}
+		}
+}
+int bubble_sort1(int a[],int start,int end)//冒泡排序法，小→大
+{
+	int i,j,k;
+	for(i=end;i>start;i--)
+		for(j=start;j<i;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				k=a[j];
+				a[j]=a[j+1];
+				a[j+1]=k;
+			}
+		}
+}
+int bubble_sort2(int a[],int start,int end)//冒泡排序法，大→小
+{
+	int i,j,k;
+	for(i=end;i>start;i--)
+		for(j=start;j<i;j++)
+		{
+			if(a[j]<a[j+1])
+			{
+				k=a[j];
+				a[j]=a[j+1];
+				a[j+1]=k;
+			}
+		}
+}
+int max(int x,int y)//返回最大值
+{
+	if(x>y)
+		return x;
+	else
+		return y;
 }
